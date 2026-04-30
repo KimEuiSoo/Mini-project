1afe521c5b3f97230e3dca055fec9bd8f9b0ffbb
KimEuiSoo <dmltn3426@naver.com>
2026-04-30 15:34:58 +0900

    feat : 드래그 앤 드롭 - back : 파일 업로드 되면 업로드 되게끔 - front : 파일 업로드 시 formData로 파일 업로드 하기(연동 테스트 중)

--- python/app.py
+++ python/app.py
@@ -1,16 +1,30 @@
+from fastapi import FastAPI, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi import FastAPI
+import shutil
+import os
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:3000"],  # React 주소
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-@app.get("/")
-def read_root():
-    return {"message":"crypto"}
+UPLOAD_DIR = "uploads"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
+
+@app.post("/upload")
+async def upload_file(file: UploadFile = File(...)):
+    file_path = os.path.join(UPLOAD_DIR, file.filename)
+
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    return {
+        "message": "업로드 성공",
+        "filename": file.filename,
+        "path": file_path
+    }
--- react/mini-app/src/component/home/HomePage.tsx
+++ react/mini-app/src/component/home/HomePage.tsx
@@ -1,19 +1,50 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import {Logo} from '../../src/logo/Logo'
+import './hompage.css';
 
 const HomePage = () => {
     const [message, setMessage] = useState("")
+    const [isActive, setActive] = useState(false);
+    const handleDragStart = () => setActive(true);
+    const handleDragEnd = () => setActive(false);
+    
+    const handleDrop = async (event: React.DragEvent) => {
+        event.preventDefault();
 
-    useEffect(()=>{
-        axios.get("http://localhost:8000/")
-            .then(res=>setMessage(res.data.message))
-            .catch(err=>console.error(err));
-    },[])
+        const file = event.dataTransfer.files[0];
+            if (!file) return;
+
+        const result = await uploadFile(file);
+        console.log(result);
+    };
+
+    const uploadFile = async (file: File) => {
+        const formData = new FormData();
+        formData.append("file", file);
+
+        const res = await fetch("http://localhost:8000/upload", {
+            method: "POST",
+            body: formData,
+        });
+
+        if (!res.ok) {
+            throw new Error("업로드 실패");
+        }
+        return await res.json();
+    };
 
     return(
         <div>
-            <h1>FastAPI 연동</h1>
-            <p>FastAPI 파라미터 : {message}</p>
+            <label className={`preview${isActive ? ' active' : ''}`}  // isActive 값에 따라 className 제어
+                   onDragEnter={handleDragStart}  // dragstart 핸들러 추가
+                   onDragLeave={handleDragEnd}  // dragend 핸들러 추가
+                   onDrop={handleDrop}>
+                <input type="file" className="file" />
+                <Logo />
+                <p className="preview_msg">클릭 혹은 파일을 이곳에 드롭하세요.</p>
+                <p className="preview_desc">파일당 최대 3MB</p>
+            </label>
         </div>
     )
 }
